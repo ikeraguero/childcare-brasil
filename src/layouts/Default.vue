bc27b968efb801e9aef57bc79219ef0d45cdeaa3
ikeraguero <ikerpires407@gmail.com>
2021-11-24 15:52:19 -0300

    Projeto-adocao

---
+++ src/layouts/Default.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark height="100">
+      <v-spacer></v-spacer>
+      <v-icon large>mdi-account</v-icon>
+      <v-app-bar-nav-icon large @click.stop="sidebar = !sidebar"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer right app v-model="sidebar" :mini-variant.sync="mini">
+      <v-list dense color="primary" dark height="100">
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon @click.stop="sidebar = !sidebar">mdi-chevron-right</v-icon>
+          </v-list-item-action>
+          <v-list-item-title>
+            <h3 class="pa-9" >Minha Conta</h3>
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+      <v-list-item class="px=2" @click="mini = !mini">
+        <v-list-item-avatar>
+          <v-icon standard>mdi-account</v-icon>
+        </v-list-item-avatar>
+        <v-list-item-content>Usuário</v-list-item-content>
+        <v-btn icon small><v-icon>mdi-chevron-right</v-icon></v-btn>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item v-for="item of items" :key="item.title" link :to="item.to">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>{{ item.title }}</v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+    <v-footer app class="py-3"
+      ><span class="caption">Todos os direitos reservados &copy;2021</span></v-footer
+    >
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sidebar: true,
+      mini: false,
+      items: [
+        { title: "Home", icon: "mdi-home", to: "/" },
+        { title: "Perfil", icon: "mdi-account-cog", to: "/perfil" },
+        { title: "Quem somos", icon: "mdi-account-group", to: "/quemSomos" },
+        { title: "Contato", icon: "mdi-contacts", to: "/contato" },
+        { title: "Doações", icon: "mdi-cash", to: "/doacoes" },
+        { title: "FAQ", icon: "mdi-help-circle", to: "/faq" },
+        { title: "Sair", icon: "mdi-exit-to-app", to: "/login" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,50 +7,52 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: () => import('@/layouts/Default'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: Home
+      },
+      {
+        path: '/perfil',
+        name: 'Perfil',
+        component: () => import(/* webpackChunkName: "perfil" */ '../views/Perfil.vue')
+      },
+      {
+        path: '/quemsomos',
+        name: 'Quem Somos',
+        component: () => import(/* webpackChunkName: "quemsomos" */ '../views/QuemSomos.vue')
+      },
+      {
+        path: '/contato',
+        name: 'Contato',
+        component: () => import(/* webpackChunkName: "contato" */ '../views/Contato.vue')
+      },
+      {
+        path: '/doacoes',
+        name: 'Doacoes',
+        component: () => import(/* webpackChunkName: "doacoes" */ '../views/Doacoes.vue')
+      },
+      {
+        path: '/faq',
+        name: 'FAQ',
+        component: () => import(/* webpackChunkName: "faq" */ '../views/FAQ.vue')
+      },
+    ]
   },
   {
-    path: '/perfil',
-    name: 'Perfil',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Perfil.vue')
-  },
-  {
-    path: '/quemSomos',
-    name: 'QuemSomos',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/QuemSomos.vue')
-  },
-  {
-    path: '/contato',
-    name: 'Contato',
-    component:()=> import ('../views/Contato.vue')
-  },
-  {
-    path: '/doacoes',
-    name: 'Doacoes',
-    component:()=> import ('../views/Doacoes.vue')
-  },
-  {
-    path: '/faq',
-    name: 'FAQ',
-    component:()=> import ('../views/FAQ.vue')
-  },
-  {
-    path: '/sair',
-    name: 'Sair',
-    component:()=> import ('../views/Sair.vue')
-  },
-
-  
-
+    path: '/',
+    component: () => import('@/layouts/Blank'),
+    children: [
+      {
+        path: '/login',
+        name: 'Login',
+        component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+      }
+    ]
+  }
 ]
-
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
