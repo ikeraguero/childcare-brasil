6967a7365d3310a92dbb140526541f53213e422b
ikeraguero <ikeraguero407@gmail.com>
2022-01-17 16:32:27 -0300

    add-firebase

--- src/components/adocao/ListarAnimais.vue
+++ src/components/adocao/ListarAnimais.vue
@@ -8,7 +8,7 @@
       sm="4"
     >
       <v-card flat tile class="d-flex" @click="$emit('adotar', animal)">
-        <v-img :src="animal.src" aspect-ratio="1" class="grey lighten-2">
+        <v-img :src='animal.src' aspect-ratio="1" class="grey lighten-2">
           <template v-slot:placeholder>
             <v-row class="fill-height ma-0" align="center" justify="center">
               <v-progress-circular
@@ -24,11 +24,40 @@
 </template>
 
 <script>
+import { getApp } from "firebase/app";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 export default {
   props: {
     value: {
       type: Array,
       required: true,
+    },
+  },
+  // created() {
+  //   this.getImage();
+  // },
+  methods: {
+    async getImage() {
+      const firebaseApp = getApp();
+      const storage = getStorage(firebaseApp);
+      for (let animal of this.value) {
+        try {
+          const url = await getDownloadURL(
+            ref(storage, `animais/${animal.id}.jpg`)
+          );
+          const xhr = new XMLHttpRequest();
+          xhr.responseType = "blob";
+          xhr.onload = () => {
+            xhr.response;
+          };
+          xhr.open("GET", url);
+          xhr.send();
+
+          animal.src = url;
+        } catch (error) {
+          console.log(error);
+        }
+      }
     },
   },
 };
