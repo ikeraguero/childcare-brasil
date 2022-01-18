a5cc475b34d518ff62e7471caaf652e587f72933
ikeraguero <ikerpires407@gmail.com>
2022-01-18 01:46:51 -0300

    atualizando

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,20 +1,28 @@
 <template>
   <v-container class="pa-8">
       <h1 class="titulo">Perguntas Frequentes</h1>
-      <h2 class="titulo-faq">Pergunta 1 </h2>
-      <p class="texto">Lorem ipsum platea non hac primis augue semper etiam et, volutpat ut scelerisque varius sit pretium curae potenti. </p>
+      <h2 class="titulo-faq">Quem pode realizar uma adoção?</h2>
+      <p class="texto">Qualquer pessoa maior de 18 anos, que tenha renda para manter o pet em bons estados e muito amor para dar. </p>
 
-      <h2 class="titulo-faq">Pergunta 2 </h2>
-      <p class="texto">Lorem ipsum platea non hac primis augue semper etiam et, volutpat ut scelerisque varius sit pretium curae potenti. </p>
+      <h2 class="titulo-faq">O que é preciso para adotar um pet?</h2>
+      <p class="texto">É preciso, como já dito anteriormente, ter a verba para manter o pet e um cantinho na sua casa para o animalzinho. Assim como tempo para dar amor, carinho e atenção para o pet. </p>
 
-      <h2 class="titulo-faq">Pergunta 3 </h2>
-      <p class="texto">Lorem ipsum platea non hac primis augue semper etiam et, volutpat ut scelerisque varius sit pretium curae potenti. </p>
+      <h2 class="titulo-faq">Quais os documentos necessários? </h2>
+      <p class="texto">CPF, RG e comprovante de residência. </p>
 
-      <h2 class="titulo-faq">Pergunta 4 </h2>
-      <p class="texto">Lorem ipsum platea non hac primis augue semper etiam et, volutpat ut scelerisque varius sit pretium curae potenti. </p>
+      <h2 class="titulo-faq">Fui aprovado(a), posso retirar o pet no mesmo dia?</h2>
+      <p class="texto">Claro que pode, desde que informe ao menos 2 horas antes da retirada. </p>
 
-      <h2 class="titulo-faq">Pergunta 5 </h2>
-      <p class="texto">Lorem ipsum platea non hac primis augue semper etiam et, volutpat ut scelerisque varius sit pretium curae potenti. </p>
+      <h2 class="titulo-faq">Os animais que vão pra adoção são castrados?</h2>
+      <p class="texto">Sim. Nossa ong possui parceria com uma clínica veterinária, que possui base em nossas filiais, assim todos os animais que chegam ao nosso centro são castrados em no máximo 3 dias. Do mesmo modo que são castrados, os animais tomam todas as vacinas faltantes, e são colocados para adoção com todas as vacinas já tomadas. </p>
+      
+      <h2 class="titulo-faq">Tem alguma taxa para adoção?</h2>
+      <p class="texto">Não, como somos uma organização não governamental e sem fins lucrativos, adotar os animaizinhos é totalmente gratuito. </p>
+      
+      <h2 class="titulo-faq">É possível a devolução do pet?</h2>
+      <p class="texto">Sim a devolução é possível, porém fica a caráter do adotante realizar a adoção já tendo em mente os possíveis contratempos que podem enfrentar ao adotar um pet.
+
+ </p>
   </v-container>
 </template>
 
