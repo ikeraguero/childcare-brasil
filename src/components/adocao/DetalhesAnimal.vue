526bc6e3408d8227f0aa5c81e5872dbca9c63499
ikeraguero <ikeraguero407@gmail.com>
2022-01-20 15:46:24 -0300

    add-firebase-detalhes

--- src/components/adocao/DetalhesAnimal.vue
+++ src/components/adocao/DetalhesAnimal.vue
@@ -15,41 +15,40 @@
       </v-card>
     </v-col>
     <v-col>
-
-    <v-col>
-      <v-row class="caracteristicas">
-        <div class="">
-          <h1><p>{{ value.nome }}</p></h1>
-          <p>Animal: {{ value.animal }}</p>
-          <p>Idade: {{ value.idade }}</p>
-          <p>Sexo: {{ value.sexo }}</p>
-          <p>Raça: {{ value.raca }}</p>
-          <p>Porte: {{ value.porte }}</p>
-          <p>Descrição: {{ value.descricao}}</p>
-        </div>
-        <v-col class="d-flex justify-end">
-          <v-row class="d-flex justify-center ">
-            <v-btn  class="config-botao" max-width="230" link to="/formulario">
-              <v-img
-              lazy-src="@/assets/imagens/queroadotar.png"
-              src="@/assets/imagens/queroadotar.png"
-              max-height="350px"
-              max-width="400px"
-            ></v-img>
-            </v-btn>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-col>
-  
-      
-
-
+      <v-col>
+        <v-row class="caracteristicas">
+          <div class="">
+            <h1>
+              <p>{{ value.nome }}</p>
+            </h1>
+            <p>Animal: {{ value.Animal }}</p>
+            <p>Idade: {{ value.idade }}</p>
+            <p>Sexo: {{ value.sexo }}</p>
+            <p>Raça: {{ value.raca }}</p>
+            <p>Porte: {{ value.porte }}</p>
+            <p>Descrição: {{ value.Descrição }}</p>
+          </div>
+          <v-col class="d-flex justify-end">
+            <v-row class="d-flex justify-center">
+              <v-btn class="config-botao" max-width="230" link to="/formulario">
+                <v-img
+                  lazy-src="@/assets/imagens/queroadotar.png"
+                  src="@/assets/imagens/queroadotar.png"
+                  max-height="350px"
+                  max-width="400px"
+                ></v-img>
+              </v-btn>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-col>
     </v-col>
   </v-row>
 </template>
 
 <script>
+
+
 export default {
   props: {
     value: {
@@ -57,26 +56,25 @@ export default {
       required: true,
     },
   },
+
 };
 </script>
 
 <style>
-.titulo{
+.titulo {
   font-size: 60px;
-  font-family:'Reenie Beanie', cursive;
+  font-family: "Reenie Beanie", cursive;
   color: #f8f7f2;
 }
-.caracteristicas{
+.caracteristicas {
   font-size: 25px;
-  font-family: 'Cardo';
+  font-family: "Cardo";
   color: #f8f7f2;
   text-justify: auto;
   padding-top: 31x;
 }
 
-.config-botao{
+.config-botao {
   margin-top: 120px;
 }
-
-
 </style>
--- src/views/QueroAdotar.vue
+++ src/views/QueroAdotar.vue
@@ -12,6 +12,9 @@
 import ListarAnimais from "@/components/adocao/ListarAnimais.vue";
 import DetalhesAnimal from "@/components/adocao/DetalhesAnimal.vue";
 
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/plugins/firebase";
+
 export default {
   name: "QueroAdotar",
   data() {
@@ -19,106 +22,7 @@ export default {
       detalhes: false,
       animalSelecionado: {},
       items: [
-        {
-          id: 1,
-          src: require("@/assets/imagens/1.jpg"),
-          animal: "Gato",
-          nome: "Pandora",
-          idade: "2 anos",
-          sexo: "Fêmea",
-          raca: "Vira Lata",
-          porte: "Medio",
-          descricao: "Pet carinhosa",
-
-        },
-        {
-          id: 2,
-          src: require("@/assets/imagens/2.jpg"),
-          animal: "Cachorro",
-          nome: "Belinha",
-          idade: "4 anos",
-          sexo: "Fêmea",
-          raca: "Vira Lata",
-          porte: "Grande",
-          descricao: "Pet amável",
-        },
-        {
-          id: 3,
-          src: require("@/assets/imagens/3.jpg"),
-          animal: "Gato",
-          nome: "Nina",
-          idade: "6 anos",
-          sexo: "Fêmea",
-          raca: "Vira Lata",
-          porte: "Pequeno",
-          descricao: "Pet tranquila e companheira",
-        },
-        {
-          id: 4,
-          src: require("@/assets/imagens/4.jpg"),
-          animal: "Cachorro",
-          nome: "Princesa",
-          idade: "8 anos",
-          sexo: "Fêmea",
-          raca: "Vira Lata",
-          porte: "Medio",
-          descricao: "Pet carinhosa e brincalhona",
-        },
-        {
-          id: 5,
-          src: require("@/assets/imagens/5.png"),
-          animal: "Gato",
-          nome: "Hunter",
-          idade: "3 anos",
-          sexo: "Macho",
-          raca: "Vira Lata",
-          porte: "Médio",
-          descricao: "Pet arisco, mas muito companheiro",
-        },
-        {
-          id: 6,
-          src: require("@/assets/imagens/6.jpg"),
-          animal: "Cachorro",
-          nome: "Zoey",
-          idade: "9 anos",
-          sexo: "Fêmea",
-          raca: "Vira Lata",
-          porte: "Grande",
-          descricao: "Pet muito carinhosa",         
-        },
-        {
-          id: 7,
-          src: require("@/assets/imagens/7.jpg"),
-          animal: "Gato",
-          nome: "Lua",
-          idade: "3 anos",
-          sexo: "Fêmea",
-          raca: "Vira Lata",
-          porte: "Pequeno",
-          descricao: "Pet calma e fácil de lidar",        
-        },
-        {
-          id: 8,
-          src: require("@/assets/imagens/8.jpg"),
-          animal: "Cachorro",
-          nome: "Chloe",
-          idade: "3 anos",
-          sexo: "Fêmea",
-          raca: "Vira Lata",
-          porte: "Pequeno",
-          descricao: "Pet calma e fácil de lidar",
-        },
-        {
-          id: 9,
-          src: require("@/assets/imagens/9.jpg"),
-          animal: "Gato",
-          nome: "Pikki",
-          idade: "3 anos",
-          sexo: "Macho",
-          raca: "Vira Lata",
-          porte: "Pequeno",
-          descricao: "Pet calmo e tranquilo",
-        },
+       
       ],
     };
   },
@@ -128,6 +32,13 @@ export default {
       this.animalSelecionado = animal;
       this.detalhes = true;
     },
+  },
+    async created() {
+    const querySnapshot = await getDocs(collection(db, "animais"));
+    querySnapshot.forEach((doc) => {
+      const src = doc.data().src
+      this.items.push({id: doc.id, ...doc.data(), src: require(`@/assets/imagens/${src}`)})
+    });
   },
 };
 </script>
