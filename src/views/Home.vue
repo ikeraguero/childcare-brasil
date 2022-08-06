84972ab262607d8924adfd32a711b830c0afcf5d
ikeraguero <ikerpires407@gmail.com>
2022-08-06 17:22:46 -0300

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,10 +13,10 @@
         <br><br>
         
 
-          <div class="d-flex justify-space-around mt-20 ">
+          <div class="d-flex justify-space-around mt-20 " v-if="$auth.isAuthenticated">
 
-            <v-btn icon min-width="200" link to="/login">
-            <router-link to="/login" v-if="$auth.isnotAuthenticated"></router-link>  
+            <v-btn icon min-width="200" link to="/ajudarcriancas">
+              
             <v-img 
             class="posicao"
             lazy-src="@/assets/imagens/3.png"
@@ -36,6 +36,31 @@
           ></v-img>
             </v-btn> 
           </div>
+          <div class="d-flex justify-space-around mt-20 " v-if="!$auth.isAuthenticated">
+
+            <v-btn icon min-width="200" link to="/login">
+              
+            <v-img 
+            class="posicao"
+            lazy-src="@/assets/imagens/3.png"
+            max-height="300px"
+            max-width="350px"
+            src="@/assets/imagens/1a.png" 
+          ></v-img>
+            </v-btn>
+            <v-btn icon min-width="200" link to="/login">
+
+            <v-img
+            class="posicao"
+            lazy-src="@/assets/imagens/4.png"
+            max-height="300"
+            max-width="350"
+            src="@/assets/imagens/2a.png"
+          ></v-img>
+            </v-btn> 
+          </div>
+
+          
           <br>
           <div class="d-flex justify-space-around mt-16 text-3xl" height="100">
             <h1>Crianças</h1> 
