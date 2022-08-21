7310bbc8d58c78d570fd5f3275583edc125bfea9
ikeraguero <ikerpires407@gmail.com>
2022-08-21 17:58:12 -0300

    Contact Page

--- src/views/Contato.vue
+++ src/views/Contato.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-container class="pa-8">
+  <v-container class="pa-8 content-align-center">
+    <div>
+          Deseja registrar crianças e/ou escolas em nosso site? Entre em contato conosco em:
+    </div>
+  <div class="align-itens-center">
+    <u>childcarebrasil@gmail.com</u>
+  </div>
   </v-container>
 </template>
 
@@ -10,4 +16,10 @@ export default {
 </script>
 
 <style>
+.pa-8 {
+  color: #FFF6EE;
+  font-size: 20px;
+  font-family: "Proxima Nova", system-ui, sans-serif;
+  text-align: center;
+}
 </style>
