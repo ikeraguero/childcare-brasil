66a6c7e88df057268fd9e7cf597ad39e48325da9
ikeraguero <ikerpires407@gmail.com>
2022-08-27 21:01:28 -0300

    update

--- src/views/comoDoar.vue
+++ src/views/comoDoar.vue
@@ -1,32 +1,175 @@
 <template>
-  <v-timeline
-    align-top
-    :dense="$vuetify.breakpoint.smAndDown"
-  >
+  <v-timeline :dense="$vuetify.breakpoint.smAndDown">
     <v-timeline-item
-      v-for="(item, i) in items"
-      :key="i"
-      :color="item.color"
-      :icon="item.icon"
+      color="#FFF6EE"
       fill-dot
+      right
+      small
     >
-      <v-card
-        :color="item.color"
-        dark
-      >
-        <v-card-title class="text-h6">
-          Lorem Ipsum Dolor
-        </v-card-title>
-        <v-card-text class="white text--primary">
-          <p>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</p>
-          <v-btn
-            :color="item.color"
-            class="mx-0"
-            outlined
+      <v-card>
+        <v-card-title class="brown lighten-1">
+          <v-icon
+            dark
+            size="42"
+            class="mr-4"
           >
-            Button
-          </v-btn>
-        </v-card-text>
+          </v-icon>
+          <h2 class="text-h4 white--text font-weight-light">
+            Title 1
+          </h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col
+              cols="12"
+              md="10"
+            >
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
+            </v-col>
+            <v-col
+              class="hidden-sm-and-down text-right"
+              md="2"
+            >
+              <v-icon size="64">
+
+              </v-icon>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="#FFF6EE"
+      fill-dot
+      left
+      small
+    >
+      <v-card>
+        <v-card-title class="brown lighten-1">
+          <h2 class="text-h4 mr-4 white--text font-weight-light">
+            Title 2
+          </h2>
+          <v-icon
+            dark
+            size="42"
+          >
+          </v-icon>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col
+              cols="12"
+              md="8"
+            >
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit.
+            </v-col>
+            <v-col
+              cols="12"
+              md="4"
+            >
+              Lorem ipsum dolor sit amet, no nam oblique veritus.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="#FFF6EE"
+      fill-dot
+      right
+      small
+    >
+      <v-card>
+        <v-card-title class="brown lighten-1">
+          <v-icon
+            class="mr-4"
+            dark
+            size="42"
+          >
+          </v-icon>
+          <h2 class="text-h4 white--text font-weight-light">
+            Title 3
+          </h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col
+              v-for="n in 3"
+              :key="n"
+              cols="12"
+              md="4"
+            >
+              Lorem ipsum dolor sit amet, no nam oblique veritus no nam oblique.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="#FFF6EE"
+      fill-dot
+      left
+      small
+    >
+      <v-card>
+        <v-card-title class="brown lighten-1">
+          <h2 class="text-h4 mr-4 white--text font-weight-light">
+            Title 4
+          </h2>
+          <v-icon
+            dark
+            size="42"
+          >
+          </v-icon>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col
+              class="hidden-sm-and-down"
+              md="2"
+            >
+              <v-icon size="64">
+              </v-icon>
+            </v-col>
+            <v-col
+              cols="12"
+              md="10"
+            >
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus.
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-timeline-item>
+
+    <v-timeline-item
+      color="#FFF6EE"
+      fill-dot
+      right
+      small
+    >
+      <v-card>
+        <v-card-title class="brown lighten-1">
+          <v-icon
+            class="mr-4"
+            dark
+            size="42"
+          >
+          </v-icon>
+          <h2 class="text-h4 white--text font-weight-light">
+            Title 5
+          </h2>
+        </v-card-title>
+        <v-container>
+          <v-row>
+            <v-col>
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
+            </v-col>
+          </v-row>
+        </v-container>
       </v-card>
     </v-timeline-item>
   </v-timeline>
