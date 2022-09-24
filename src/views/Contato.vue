a25b5ff9ccea02aa646128fe1c6f26e5d01229bb
ikeraguero <ikerpires407@gmail.com>
2022-09-24 17:04:35 -0300

    Contact Page

--- src/views/Contato.vue
+++ src/views/Contato.vue
@@ -1,25 +1,45 @@
 <template>
   <v-container class="pa-8 content-align-center">
-    <div>
+    <div class="mt-20 font-medium">
+      <h1>Redes Sociais do ChildcareBrasil: </h1>
+          <div class="icons flex justify-center space-x-24 mt-10 text-base">
+            <li><Icon icon="entypo-social:twitter" color="#fff6ee" width="70" height="70"/>Twitter</li>
+            <li><Icon icon="entypo-social:instagram" color="#fff6ee" width="70" height="70"/>Instagram</li>
+            <li><Icon icon="entypo-social:facebook" color="#fff6ee" width="70" height="70" />Facebook</li>
+          </div>
+    </div>
+    <div class="mt-36">
           Deseja registrar crianças e/ou escolas em nosso site? Entre em contato conosco em:
     </div>
   <div class="align-itens-center">
     <u>childcarebrasil@gmail.com</u>
   </div>
+
+
+
+
+
   </v-container>
 </template>
 
 <script>
+import { Icon } from '@iconify/vue2';
+
 export default {
-
-}
+	components: {
+		Icon,
+	},
+};
 </script>
-
-<style>
+<style >
 .pa-8 {
   color: #FFF6EE;
   font-size: 26px;
   font-family: "Proxima Nova", system-ui, sans-serif;
   text-align: center;
+}
+
+.icons {
+  list-style: none;
 }
 </style>
