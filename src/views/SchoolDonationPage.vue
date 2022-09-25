76adf03c5aa94db1f92606e467ad80c1c030625c
ikeraguero <ikerpires407@gmail.com>
2022-09-25 11:01:22 -0300

    Donation Forms into Components

---
+++ src/views/SchoolDonationPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="block galleryBlock">
+
+        <SchoolsDonationForm class="flex drop-shadow-md mb-9" />
+
+    </div>
+  </template>
+  
+  <script>
+  
+  
+  
+  export default {
+        name:"Escola",
+        components: {
+  
+      },
+    }
