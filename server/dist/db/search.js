fc6edc26eb6305ff001a7307b607ef508168a8c2
ikeraguero <ikerpires407@gmail.com>
2024-01-15 20:00:25 -0300

    Prisma Bug

--- server/dist/db/search.js
+++ server/dist/db/search.js
@@ -1,144 +1,208 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getState = exports.getStates = exports.getDonation = exports.getDonations = exports.getSchool = exports.getSchools = exports.getChild = exports.getChildren = void 0;
+exports.getState =
+  exports.getStates =
+  exports.getDonation =
+  exports.getDonations =
+  exports.getSchool =
+  exports.getSchools =
+  exports.getChild =
+  exports.getChildren =
+    void 0;
 const responseConstructor_1 = require("../lib/responseConstructor");
-const client_1 = require("@prisma/client");
+const client_1 = require("@prisma/client/edge");
 const prisma = new client_1.PrismaClient();
 // get all children
 function getChildren() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield prisma.$connect();
-        const children = yield prisma.child.findMany();
-        if (!children) {
-            prisma.$disconnect();
-            return new responseConstructor_1.response(503, "Children not found");
-        }
-        yield prisma.$disconnect();
-        return new responseConstructor_1.response(200, "Children found", children);
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    yield prisma.$connect();
+    const children = yield prisma.child.findMany();
+    if (!children) {
+      prisma.$disconnect();
+      return new responseConstructor_1.response(503, "Children not found");
+    }
+    yield prisma.$disconnect();
+    return new responseConstructor_1.response(200, "Children found", children);
+  });
 }
 exports.getChildren = getChildren;
 //get specific child
 function getChild(child_id) {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield prisma.$connect();
-        const child = yield prisma.child.findUnique({
-            where: {
-                id: child_id,
-            },
-        });
-        if (!child) {
-            prisma.$disconnect();
-            return new responseConstructor_1.response(503, `Child ${child_id} not found!`);
-        }
-        yield prisma.$disconnect();
-        return new responseConstructor_1.response(200, `Child ${child_id} found!`, child);
+  return __awaiter(this, void 0, void 0, function* () {
+    yield prisma.$connect();
+    const child = yield prisma.child.findUnique({
+      where: {
+        id: child_id,
+      },
     });
+    if (!child) {
+      prisma.$disconnect();
+      return new responseConstructor_1.response(
+        503,
+        `Child ${child_id} not found!`
+      );
+    }
+    yield prisma.$disconnect();
+    return new responseConstructor_1.response(
+      200,
+      `Child ${child_id} found!`,
+      child
+    );
+  });
 }
 exports.getChild = getChild;
 //get all schools
 function getSchools() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield prisma.$connect();
-        const schools = yield prisma.school.findMany();
-        if (!schools) {
-            prisma.$disconnect();
-            return new responseConstructor_1.response(503, "Schools not found");
-        }
-        yield prisma.$disconnect();
-        return new responseConstructor_1.response(200, "Schools found", schools);
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    yield prisma.$connect();
+    const schools = yield prisma.school.findMany();
+    if (!schools) {
+      prisma.$disconnect();
+      return new responseConstructor_1.response(503, "Schools not found");
+    }
+    yield prisma.$disconnect();
+    return new responseConstructor_1.response(200, "Schools found", schools);
+  });
 }
 exports.getSchools = getSchools;
 //get specific school
 function getSchool(school_id) {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield prisma.$connect();
-        const school = yield prisma.school.findUnique({
-            where: {
-                id: school_id,
-            },
-        });
-        if (!school) {
-            prisma.$disconnect();
-            return new responseConstructor_1.response(503, `School ${school_id} not found!`);
-        }
-        yield prisma.$disconnect();
-        return new responseConstructor_1.response(200, `School ${school_id} found!`, school);
+  return __awaiter(this, void 0, void 0, function* () {
+    yield prisma.$connect();
+    const school = yield prisma.school.findUnique({
+      where: {
+        id: school_id,
+      },
     });
+    if (!school) {
+      prisma.$disconnect();
+      return new responseConstructor_1.response(
+        503,
+        `School ${school_id} not found!`
+      );
+    }
+    yield prisma.$disconnect();
+    return new responseConstructor_1.response(
+      200,
+      `School ${school_id} found!`,
+      school
+    );
+  });
 }
 exports.getSchool = getSchool;
 //get all donations
 function getDonations() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield prisma.$connect();
-        const donations = yield prisma.donation.findMany();
-        if (!donations) {
-            prisma.$disconnect();
-            return new responseConstructor_1.response(503, "Donations not found");
-        }
-        yield prisma.$disconnect();
-        return new responseConstructor_1.response(200, "Donations found", donations);
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    yield prisma.$connect();
+    const donations = yield prisma.donation.findMany();
+    if (!donations) {
+      prisma.$disconnect();
+      return new responseConstructor_1.response(503, "Donations not found");
+    }
+    yield prisma.$disconnect();
+    return new responseConstructor_1.response(
+      200,
+      "Donations found",
+      donations
+    );
+  });
 }
 exports.getDonations = getDonations;
 //get specific donation
 function getDonation(donation_id) {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield prisma.$connect();
-        const donation = yield prisma.donation.findUnique({
-            where: {
-                id: donation_id,
-            },
-        });
-        if (!donation) {
-            prisma.$disconnect();
-            return new responseConstructor_1.response(503, `Donation ${donation_id} not found!`);
-        }
-        yield prisma.$disconnect();
-        return new responseConstructor_1.response(200, `Donation ${donation_id} found!`, donation);
+  return __awaiter(this, void 0, void 0, function* () {
+    yield prisma.$connect();
+    const donation = yield prisma.donation.findUnique({
+      where: {
+        id: donation_id,
+      },
     });
+    if (!donation) {
+      prisma.$disconnect();
+      return new responseConstructor_1.response(
+        503,
+        `Donation ${donation_id} not found!`
+      );
+    }
+    yield prisma.$disconnect();
+    return new responseConstructor_1.response(
+      200,
+      `Donation ${donation_id} found!`,
+      donation
+    );
+  });
 }
 exports.getDonation = getDonation;
 //get all states
 function getStates() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield prisma.$connect();
-        const states = yield prisma.state.findMany();
-        if (!states) {
-            prisma.$disconnect();
-            return new responseConstructor_1.response(503, "States not found");
-        }
-        yield prisma.$disconnect();
-        return new responseConstructor_1.response(200, "States found", states);
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    yield prisma.$connect();
+    const states = yield prisma.state.findMany();
+    if (!states) {
+      prisma.$disconnect();
+      return new responseConstructor_1.response(503, "States not found");
+    }
+    yield prisma.$disconnect();
+    return new responseConstructor_1.response(200, "States found", states);
+  });
 }
 exports.getStates = getStates;
 //get specific state
 function getState(state_id) {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield prisma.$connect();
-        const state = yield prisma.state.findUnique({
-            where: {
-                id: state_id,
-            },
-        });
-        if (!state) {
-            prisma.$disconnect();
-            return new responseConstructor_1.response(503, `Child ${state_id} not found!`);
-        }
-        yield prisma.$disconnect();
-        return new responseConstructor_1.response(200, `Child ${state_id} found!`, state);
+  return __awaiter(this, void 0, void 0, function* () {
+    yield prisma.$connect();
+    const state = yield prisma.state.findUnique({
+      where: {
+        id: state_id,
+      },
     });
+    if (!state) {
+      prisma.$disconnect();
+      return new responseConstructor_1.response(
+        503,
+        `Child ${state_id} not found!`
+      );
+    }
+    yield prisma.$disconnect();
+    return new responseConstructor_1.response(
+      200,
+      `Child ${state_id} found!`,
+      state
+    );
+  });
 }
 exports.getState = getState;
 //# sourceMappingURL=search.js.map
--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -1,32 +1,55 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
+var __createBinding =
+  (this && this.__createBinding) ||
+  (Object.create
+    ? function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        var desc = Object.getOwnPropertyDescriptor(m, k);
+        if (
+          !desc ||
+          ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)
+        ) {
+          desc = {
+            enumerable: true,
+            get: function () {
+              return m[k];
+            },
+          };
+        }
+        Object.defineProperty(o, k2, desc);
+      }
+    : function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        o[k2] = m[k];
+      });
+var __setModuleDefault =
+  (this && this.__setModuleDefault) ||
+  (Object.create
+    ? function (o, v) {
+        Object.defineProperty(o, "default", { enumerable: true, value: v });
+      }
+    : function (o, v) {
+        o["default"] = v;
+      });
+var __importStar =
+  (this && this.__importStar) ||
+  function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    if (mod != null)
+      for (var k in mod)
+        if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k))
+          __createBinding(result, mod, k);
     __setModuleDefault(result, mod);
     return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
+const client_1 = require("@prisma/client/edge");
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const api = __importStar(require("./controllers/apicontroller"));
@@ -39,10 +62,10 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((0, cors_1.default)());
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
 });
 // GET
 app.get("/api/children", api.children);
@@ -67,6 +90,6 @@ app.put("/api/donation/:donation_id", api.donation);
 //
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Port: http://localhost:${port}.`);
+  console.log(`Port: http://localhost:${port}.`);
 });
 //# sourceMappingURL=index.js.map
